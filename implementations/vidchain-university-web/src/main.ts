0ddad65e129a2d267cfddbe5d370961a5bdf46e6
Alex Bean <alexfraga10@gmail.com>
2020-04-09 12:48:40 +0200

    frontend setuo

--- implementations/vidchain-university-web/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as config from './config';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(config.PORT);
-}
-bootstrap();
