f562f790277b8865deccafc29d106c3334354e83
Alex Bean <alexfraga10@gmail.com>
2020-04-15 18:52:17 +0200

    store/retrieve did in redis

--- implementations/vidchain-government/vidchain-government-backend/src/app.service.ts
+++ implementations/vidchain-government/vidchain-government-backend/src/app.service.ts
@@ -5,18 +5,24 @@ import { Logger } from '@nestjs/common';
 import {ValidateResponse, Signature} from "./interfaces/dtos"
 import {VidchainBackend} from "./api/vidchainBackend";
 import { parseJwt } from "./utils/Parser";
+import Redis from 'ioredis';
+
 @Injectable()
 export class AppService {
   private logger: Logger = new Logger('LoginGateway');
   private readonly socket = io(config.BASE_URL);
   private vidchainBackend:VidchainBackend = new VidchainBackend();
+  private readonly nonceRedis = new Redis({ keyPrefix: "nonce:" });
 
-  getHello(): string {
-    //var allowedOrigins = "http://localhost:* http://127.0.0.1:*";
+  async getHello(): Promise<string> {
+    var userDID = "did:ebsi:0x72D8cfaB840Bb72391DF733E10E6225b83352807";
+    const user = await this.nonceRedis.get(userDID)
+    this.logger.log(`User from DB:`)
+    this.logger.log(user)
     const socket = io(config.BASE_URL);
-    this.logger.log(`socket created:`)
-    socket.emit('login', 'hello from a client on the backend');
-    return 'Hello World!';
+    // this.logger.log(`socket created:`)
+    // socket.emit('login', 'hello from a client on the backend');
+    return user;
   }
 
   async validate(signature: Signature): Promise<ValidateResponse> {
@@ -29,18 +35,22 @@ export class AppService {
       validateReponse.response = "error";
     }
     else{
-      const user = this.getUser(signature.signature);
-      const socket = io(config.BASE_URL);
-      socket.emit('login', '{}');
+      const tokenParsed = parseJwt(signature);
+      this.storeUser(tokenParsed);
+      this.sendDataToClient(tokenParsed.did);
     }
     return validateReponse;
   }
 
-  getUser(signature){
-    var token = parseJwt(signature);
-    this.logger.log(`token:`);
-    this.logger.log(token);
+  storeUser(token){
+    this.nonceRedis.set(token.did,token.nonce)
   }
+  sendDataToClient(did){
+    const socket = io(config.BASE_URL);
+    socket.emit('login', did);
+  }
+
+  
 
  
 }
