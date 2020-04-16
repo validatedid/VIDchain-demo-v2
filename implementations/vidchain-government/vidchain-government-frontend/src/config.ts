4ec01cc94279b1f5f28a0f88dc55ff60afaba9e2
Alex Bean <alexfraga10@gmail.com>
2020-04-16 11:22:34 +0200

    ngrok urls retrieve from .env file

--- implementations/vidchain-government/vidchain-government-frontend/src/config.ts
+++ implementations/vidchain-government/vidchain-government-frontend/src/config.ts
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
+// importing .env variables
+dotenv.config();
 
 const BearerToken = "eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QiLCJqa3UiOiJodHRwczovL3dhbGxldGFwaS1kZXYudmlkY2hhaW4ubmV0L2Vic2l0cnVzdGVkYXBwL3B1YmxpYy1rZXlzLyIsImtpZCI6ImVic2ktd2FsbGV0In0.eyJzdWIiOiJDaXR5IG9mIEJhcmNlbG9uYSIsImlhdCI6MTU4NjcyMjk4OSwiZXhwIjoxNTg2ODA5Mzg5LCJhdWQiOiJlYnNpLXdhbGxldCIsImRpZCI6ImRpZDplYnNpOjB4OTFBNzQzMTUyQ2IwYjRCQ2NiMjFkYTY1ZTM4NzMyRTUwMDVlYjkzRSIsImVudGVycHJpc2VOYW1lIjoiQ2l0eSBvZiBCYXJjZWxvbmEiLCJub25jZSI6ImJhcmNlbG9uYSJ9.D9nkns9ZvG3QjEA-n-Ng6Gba2hW05jVrYHcX1SWfLEdWi9qbFwq4IpP6qLIhF_oku59334t67nkJq-650HHCeQ";
 const DID = "did:ebsi:0x91A743152Cb0b4BCcb21da65e38732E5005eb93E";
 const Name = "City of Barcelona";
 const nonce = "barcelona";
 
-const BACKEND_URL= "http://localhost:3021"
+const BACKEND_URL= process.env.BACKEND_URL
 const API_URL = "https://walletapi-dev.vidchain.net/wallet/";
 
 export { 
