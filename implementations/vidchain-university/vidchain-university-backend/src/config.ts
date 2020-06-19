5763090d4617043f5cc1b395bc5b10b8b9c12e27
maurolucc <maurolucchinidepompa@gmail.com>
2020-06-19 09:57:37 +0200

    utils

--- implementations/vidchain-university/vidchain-university-backend/src/config.ts
+++ implementations/vidchain-university/vidchain-university-backend/src/config.ts
@@ -1,5 +1,5 @@
-const PORT = 3023;
-const API_URL = "https://walletapi-dev.vidchain.net/wallet/";
+const PORT = process.env.PORT || 3021;
+const API_URL = "https://dev.api.vidchain.net/api/v1/";
 const CLIENT_URL = "http://localhost:3024";
 const BASE_URL = "http://localhost:3023";
 
--- implementations/vidchain-university/vidchain-university-backend/src/utils/Parser.ts
+++ implementations/vidchain-university/vidchain-university-backend/src/utils/Parser.ts
@@ -1,4 +1,6 @@
 var jwtDecode = require('jwt-decode');
+import {decode as atob, encode } from 'base-64';
+
 /**
  * Parse a JWT token
  */
@@ -12,4 +14,31 @@ function parseJwt(token) {
   }
 }
 
-export { parseJwt };
+/**
+ * Decodes a Base64 string in an UTF-8 string format
+ * @param input Base64 encoded string to decode
+ */
+function strB64dec(input) {
+  try {
+    return JSON.parse(atob(input));
+  }
+  catch (error) {
+    return null;
+  }
+}
+
+/**
+ * Encoded  a Base64 string in an UTF-8 string format
+ * @param input Base64 encoded string to decode
+ */
+function strB64enc(input) {
+  try {
+    return encode(JSON.stringify(input));
+  }
+  catch (error) {
+    return null;
+  }
+}
+
+
+export { parseJwt, strB64dec, strB64enc };
