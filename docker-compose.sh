141dfa61fb7db56ab0841692e57d12efbaf4ae90
maurolucc <maurolucchinidepompa@gmail.com>
2020-06-25 10:42:00 +0200

    attempt to test deployment in legacy server

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,2 +1,4 @@
 docker-compose -f implementations/vidchain-government/vidchain-government-frontend/docker-compose.yml up -d --build
 docker-compose -f implementations/vidchain-government/vidchain-government-backend/docker-compose.yml up -d --build
+docker-compose -f implementations/vidchain-university/vidchain-university-frontend/docker-compose.yml up -d --build
+docker-compose -f implementations/vidchain-university/vidchain-university-backend/docker-compose.yml up -d --build
