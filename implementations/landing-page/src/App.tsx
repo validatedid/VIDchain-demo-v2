a85999ca22824f41b53c703887441ff68b5be7cd
Alejandro Bean <alexfraga10@gmail.com>
2020-08-04 22:31:33 +0200

    complete all texts

--- implementations/landing-page/src/App.tsx
+++ implementations/landing-page/src/App.tsx
@@ -5,11 +5,12 @@ import './App.css';
 
 const logoVidchain = require("./assets/images/VIDchain_horizontal.png");
 const logoValidated = require("./assets/images/validated_white.png");
-const logoNotification = require("./assets/images/VIDchain_vertical.png");
+const logoApp = require("./assets/images/VIDchain_vertical.png");
 const logoIOS = require("./assets/images/appleAppStore@3x.png");
 const logoAndroid = require("./assets/images/playStore@3x.png");
 const logoUniversity = require("./assets/images/logo_uni.png");
 const logoGovernment = require("./assets/images/logoCity.png");
+const logoNotification = require("./assets/images/icon_notification.png");
 
 function App() {
   return (
@@ -26,95 +27,73 @@ function App() {
         <Timeline />
         <div className="step-body">
           <div className="center">
-            <a className="desktop_start" href="faber.html">
+            <a className="desktop_start" href="/government">
               <div className="desktop_start_btn">Start Tutorial</div>
             </a>
-            <a className="mobile_start" href="faber.html">
+            <a className="mobile_start" href="/government">
               <div className="mobile_start_btn">Start Tutorial</div>
             </a>
           </div>
-          <h2>Getting Started with Connect.Me</h2>
+          <h2>Welcome to the VIDchain demo tutorial</h2>
           <p>
-            This is an optional tutorial designed to show you what digital
-            credentials are, and how using Connect.Me can simplify, speed up and
-            secure your online interactions.
+            Test the latest demo, downloading our app and using it following the instructions.
           </p>
-
-          <p>
-            If you don't already have Connect.Me installed, download it now to
-            your mobile phone.
-          </p>
+          <div className="links" style={{height: "96px"}}>
+            <img src={logoApp} style={{marginRight: "50px", width:"121px"}} />
+            <div className="playstore">
+              <a id="x" href="https://itunes.apple.com">
+                <img src={logoIOS}style={{width:"121px",marginRight: "20px"}} />
+              </a>
+              <a href="https://play.google.com/store/">
+                <img src={logoAndroid} style={{width:"121px"}} />
+              </a>
+            </div>
         </div>
-        <div className="links" style={{height: "96px"}}>
-          <img src={logoNotification} style={{marginRight: "20px", width:"120px"}} />
-          <div className="playstore">
-            <a id="x" href="https://itunes.apple.com">
-              <img src={logoIOS}style={{width:"121px",marginRight: "20px"}} />
-            </a>
-            <a href="https://play.google.com/store/">
-              <img src={logoAndroid} style={{width:"121px"}} />
-            </a>
-          </div>
-        </div>
-        <img className="step1_mobile" style={{marginBottom: "0px",width: "269px"}} src="./img/a-m@3x.png" alt="" />
 
-        <p className="clearfix" style={{marginTop: "24px", color: "#393939", fontSize: "17px"}}>
+        </div>
+
+        <p className="clearfix" style={{marginTop: "20px", color: "#393939", fontSize: "17px"}}>
           The tutorial consists of three parts:
         </p>
         <div className="step step-body">
           <div>
-            <h3 className="heading">Part 1 - Faber College</h3>
+            <h3 className="heading">Part 0 - VIDchain Wallet</h3>
             <p className="has-image-right">
-              Imagine you are just graduating at Faber College. Imagine they
-              offered you a way to prove your transcripts digitally and
-              anonymously, by offering you a cryptographic copy of your
-              transcripts. To hold them, you first need a digital wallet like
-              the one Connect.Me uses. Then, you will need to privately
-              'connect' with Faber College to receive your transcripts.
+              Complete the KYC process, verifying your identity document and the liveness probe to assure you are the owner of the document using the VIDchain wallet, and you will receive a Verifiable eID,
+              that you can use to identificate in other entities.
+            </p>
+          </div>
+          <img src={logoNotification} style={{width: "160px", height:"160px", marginBottom: "10px", marginRight: "25px"}} />
+        </div>
+        <div className="step step-body">
+          <div>
+            <h3 className="heading">Part 1 - Government of Freedonia</h3>
+            <p className="has-image-right">
+              Access to you government portal, using the Verifiable eID that you hold in your wallet to probe who you are.
+              Once you have access to the portal you can use the government services, and request other credentials like for example a Large Family credential that you can use in other entities to apply for discounts.
             </p>
           </div>
           <img src={logoGovernment} style={{width: "200px", height:"200px"}} />
         </div>
         <div className="step step-body">
           <div>
-            <h3 className="heading">Part 2 - ACME Corporation</h3>
+            <h3 className="heading">Part 2 - ACME University</h3>
             <p className="has-image-right">
-              Say you want to apply for a job at ACME Corp- and naturally they
-              are asking for a lot of personal information along with proof of
-              your degree. First, as before, you add ACME Corp as a connection,
-              and then over that secure relationship you can share proof of your
-              transcript you received from Faber College!
+              Access to ACME University where you are already enrolled into a course and after sign-in with VIDchain you will be able to use the university services, like request your diploma degree 
+              or apply for a discount using the Large Family credential that you got from your government.
             </p>
           </div>
           <img src={logoUniversity} style={{height:"200px", width:"200px"}} />
-        </div>
-
-        <div className="step-body">
-          <p>
-            The power of digital credentials done right is almost always
-            understated. At Evernym we believe Connect.Me represents a way for
-            individuals to truly, literally be at the center for their online
-            interactions for the first time. All your verified information
-            collected in Connect.Me stays with you. Built on Hyperledger Indy's
-            open-source code, we aim to ensure you always remain
-            'self-sovereign'. That is, to be free to take your digital things
-            and leave Connect.me and use a different digital wallet app if you
-            wish. That's the spirit of Self-Sovereign Identity! It's like being
-            able to change your email provider, but take with you all of your
-            data, contacts, history and even your same email address with you.
-            You just change providers.
-          </p>
-          <p>Welcome to the world of self-sovereign identity.</p>
         </div>
         
       </div>
     </main>
     <iframe width="100%" height="810" src="https://www.youtube.com/embed/eRxVWeX389w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <div className="center">
-          <a className="desktop_start" href="faber.html">
+          <a className="desktop_start" href="/government">
             <div className="desktop_start_btn">Start Tutorial</div>
           </a>
-          <a className="mobile_start" href="faber.html">
+          <a className="mobile_start" href="/government">
             <div className="mobile_start_btn">Start Tutorial</div>
           </a>
         </div>
