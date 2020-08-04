986bb8f6685d038e96de2ed89dcb474581d388c1
Alejandro Bean <alexfraga10@gmail.com>
2020-08-04 20:00:13 +0200

    timeline UI

--- implementations/landing-page/src/components/Timeline.tsx
+++ implementations/landing-page/src/components/Timeline.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import './Timeline.css';
 
+const logoUniversity = require("../assets/images/logo_uni.png");
+const logoGovernment = require("../assets/images/logoCity.png");
+const logoVidchain = require("../assets/images/icon_notification.png");
+
+
+
 const Timeline = ({
     
   }) => (
@@ -8,25 +14,28 @@ const Timeline = ({
         <div className="bar"></div>
         <div className="timeline">
             <div className="entry">
-                <h1>1990</h1>
-                <h2>Entry Title</h2>
-                <img src="http://dummyimage.com/300x200/000/fff" /> Here's the info about this date
+                <h1>Step 0</h1>
+                <h2>VIDchain Wallet</h2>
+                <div className="img-container">
+                    <img src={logoVidchain} className="logoTimeline"/> 
+                    <p>Install the VIDchain App<br/>and create your Verifiable eID</p>
+                </div>
             </div>
             <div className="entry">
-                <h1>1995</h1>
-                Here's the info about this date
+                <h1>Step 1</h1>
+                <h2>Government of Freedonia</h2>
+                <div className="img-container">
+                    <img src={logoGovernment} className="logoTimeline"/> 
+                    <p>Access to your government account<br/>with VIDchain and use their services</p>
+                </div>
             </div>
             <div className="entry">
-                <h1>2000</h1>
-                Here's the info about this date
-            </div>
-            <div className="entry">
-                <h1>2005</h1>
-                Here's the info about this date
-            </div>
-            <div className="entry">
-                <h1>2010</h1>
-                Here's the info about this date
+            <h1>Step 2</h1>
+                <h2>University</h2>
+                <div className="img-container">
+                    <img src={logoUniversity} className="logoTimeline"/> 
+                    <p>Access to your university portal<br/>and get your diploma</p>
+                </div>
             </div>
         </div>
     </div>
