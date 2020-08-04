c84f46e3534c93523db9573d05f51d076cbdff8b
Alejandro Bean <alexfraga10@gmail.com>
2020-08-04 09:24:41 +0200

    create-react-app

---
+++ implementations/landing-page/src/config/colors.ts
@@ -0,0 +1,24 @@
+const colors = {
+    white: '#FFF',
+    primary: '#00bf71',
+    secondary: '#888B8D',
+    blue: '#014165',
+    green: '#467A39',
+    red: '#DA2131',
+    black: '#000000',
+    grey: '#A6ACAF',
+    greyText: '#656565',
+    strongGreen: '#008f55',
+    strongGrey: '#434446',
+    middleGreen: '#33cc8d',
+    middleGrey: '#9ea0a3',
+    softGreen: '#66d9aa',
+    softGrey: '#cfcfd1',
+    greyBackground: '#E9E9E9',
+    figmaGreen1: '#00BF71',
+    figmaGreen2: '#01AF68',
+    greyBody: '#DADADA',
+    greenFab: '#009D5D',
+  };
+  
+  export default colors;
