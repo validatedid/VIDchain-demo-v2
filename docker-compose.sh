0ee330e743fa3c45a9cccb9812e773bd45e94d35
maurolucc <maurolucchinidepompa@gmail.com>
2020-08-18 12:18:33 +0200

    envs valid file names

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,8 +1,8 @@
 docker-compose -f docker-compose-redis.yml up -d
-cp ../envs/.env.vidchain-government-frontend implementations/vidchain-government/vidchain-government-frontend/.env
-cp ../envs/.env.vidchain-government-backend implementations/vidchain-government/vidchain-government-backend/.env
-cp ../envs/.env.vidchain-university-frontend implementations/vidchain-university/vidchain-university-frontend/.env
-cp ../envs/.env.vidchain-university-backend implementations/vidchain-university/vidchain-university-backend/.env
+cp /root/envs/.env.government-frontend implementations/vidchain-government/vidchain-government-frontend/.env
+cp /root/envs/.env.government-backend implementations/vidchain-government/vidchain-government-backend/.env
+cp /root/envs/.env.university-frontend implementations/vidchain-university/vidchain-university-frontend/.env
+cp /root/envs/.env.university-backend implementations/vidchain-university/vidchain-university-backend/.env
 docker-compose -f implementations/vidchain-government/vidchain-government-frontend/docker-compose.yml up -d --build
 docker-compose -f implementations/vidchain-government/vidchain-government-backend/docker-compose.yml up -d --build
 docker-compose -f implementations/vidchain-university/vidchain-university-frontend/docker-compose.yml up -d --build
