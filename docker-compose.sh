3a9fa26449ab37cddaf9f49923affa256976e556
maurolucc <maurolucchinidepompa@gmail.com>
2020-08-18 12:34:31 +0200

    relative path for envs

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,8 +1,8 @@
 docker-compose -f docker-compose-redis.yml up -d
-cp /root/envs/.env.government-frontend implementations/vidchain-government/vidchain-government-frontend/.env
-cp /root/envs/.env.government-backend implementations/vidchain-government/vidchain-government-backend/.env
-cp /root/envs/.env.university-frontend implementations/vidchain-university/vidchain-university-frontend/.env
-cp /root/envs/.env.university-backend implementations/vidchain-university/vidchain-university-backend/.env
+cp ../envs/.env.government-frontend implementations/vidchain-government/vidchain-government-frontend/.env
+cp ../envs/.env.government-backend implementations/vidchain-government/vidchain-government-backend/.env
+cp ../envs/.env.university-frontend implementations/vidchain-university/vidchain-university-frontend/.env
+cp ../envs/.env.university-backend implementations/vidchain-university/vidchain-university-backend/.env
 docker-compose -f implementations/vidchain-government/vidchain-government-frontend/docker-compose.yml up -d --build
 docker-compose -f implementations/vidchain-government/vidchain-government-backend/docker-compose.yml up -d --build
 docker-compose -f implementations/vidchain-university/vidchain-university-frontend/docker-compose.yml up -d --build
