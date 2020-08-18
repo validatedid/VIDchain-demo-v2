4fa1b495b63755ba03f9644e9302745831533a35
maurolucc <maurolucchinidepompa@gmail.com>
2020-08-18 11:54:54 +0200

    copy root directory .env files

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,4 +1,8 @@
 docker-compose -f docker-compose-redis.yml up -d
+cp .env.vidchain-government-frontend implementations/vidchain-government/vidchain-government-frontend/.env
+cp .env.vidchain-government-backend implementations/vidchain-government/vidchain-government-backend/.env
+cp .env.vidchain-university-frontend implementations/vidchain-university/vidchain-university-frontend/.env
+cp .env.vidchain-university-backend implementations/vidchain-university/vidchain-university-frontend/.env
 docker-compose -f implementations/vidchain-government/vidchain-government-frontend/docker-compose.yml up -d --build
 docker-compose -f implementations/vidchain-government/vidchain-government-backend/docker-compose.yml up -d --build
 docker-compose -f implementations/vidchain-university/vidchain-university-frontend/docker-compose.yml up -d --build
