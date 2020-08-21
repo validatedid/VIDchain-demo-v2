f309dece0f563a8cb08055bdee2787e67d2fff3a
maurolucc <maurolucchinidepompa@gmail.com>
2020-08-21 11:50:07 +0200

    landing working on kubernetes

--- implementations/landing-page/src/config/config.ts
+++ implementations/landing-page/src/config/config.ts
@@ -1,6 +1,6 @@
 const ENV = "development";
-const GOVERNMENT_URL = "https://dev.api.vidchain.net/demo/government";
-const UNIVERSITY_URL = "https://dev.api.vidchain.net/demo/university";
+const GOVERNMENT_URL = "https://dev.vidchain.net/demo/government";
+const UNIVERSITY_URL = "https://dev.vidchain.net/demo/university";
 
 export {
     GOVERNMENT_URL,
