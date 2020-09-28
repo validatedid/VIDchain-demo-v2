64412d8c7e11264de90e3a3f27aa1fc15a3ec008
maurolucc <maurolucchinidepompa@gmail.com>
2020-09-28 19:16:17 +0200

    quick fix - add REACT_APP_

--- implementations/landing-page/src/config/config.ts
+++ implementations/landing-page/src/config/config.ts
@@ -1,6 +1,6 @@
 const ENV = "development";
-const GOVERNMENT_URL = process.env.GOVERNMENT_URL || "undefined";
-const UNIVERSITY_URL = process.env.GOVERNMENT_UNIVERSITY || "undefined";
+const GOVERNMENT_URL = process.env.REACT_APP_GOVERNMENT_URL || "undefined";
+const UNIVERSITY_URL = process.env.REACT_APP_GOVERNMENT_UNIVERSITY || "undefined";
 
 export {
     GOVERNMENT_URL,
