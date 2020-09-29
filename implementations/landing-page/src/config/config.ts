cd146b3e939f6a72f2947fd99d7f83f0b8ccb770
maurolucc <maurolucchinidepompa@gmail.com>
2020-09-29 09:08:37 +0200

    fix typo

--- implementations/landing-page/src/config/config.ts
+++ implementations/landing-page/src/config/config.ts
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 // importing .env variables
 dotenv.config();
 const GOVERNMENT_URL = process.env.REACT_APP_GOVERNMENT_URL || "undefined";
-const UNIVERSITY_URL = process.env.REACT_APP_GOVERNMENT_UNIVERSITY || "undefined";
+const UNIVERSITY_URL = process.env.REACT_APP_UNIVERSITY_URL || "undefined";
 
 export {
     GOVERNMENT_URL,
