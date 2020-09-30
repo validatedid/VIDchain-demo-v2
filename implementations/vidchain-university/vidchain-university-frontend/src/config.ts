79c42988e0a3277067f231b6f7a3428bfbe5f3e6
Mauro <maurolucchinidepompa@gmail.com>
2020-09-30 16:31:27 +0200

    fix frontend env variables (#20)
    * fix typo
    
    * set REACT_APP build time
    
    * landing configmap not required
    
    * backend demo using try too
    
    * declare ARGS in frontend Dockerfiles
    
    * env files dotenv-flow gov frontend
    
    * change env files directory
    
    * dockerfile aware of ARG NODE_ENV
    
    * configmap no longer available
    
    * vars defined in azure pipeline for frontend
    
    * remove .envs
    
    * test $(REACT_APP_IDENTITY_PROVIDER)
    
    * hardcode IDENTITY_PROVIDER
    
    * set ENV variable as a global variable
    
    * INPUT_REACT_APP_IDENTITY_PROVIDER
    
    * fix indentation
    
    * --build-arg
    
    * refactor envs directory
    
    * add API_KEY from azure secret
    
    * fix indentation
    
    * remove extra steps
    
    * echo instead of cp
    
    * name without spaces
    
    * cp not echo
    
    Co-authored-by: Albert Solana <iamtxena@gmail.com>

--- implementations/vidchain-university/vidchain-university-frontend/src/config.ts
+++ implementations/vidchain-university/vidchain-university-frontend/src/config.ts
@@ -8,7 +8,7 @@ const IDENTITY_PROVIDER =
 const REDIRECT_CALLBACK =
   process.env.REACT_APP_REDIRECT_CALLBACK || "undefined";
 const BACKEND_URL =
-  process.env.REACT_APP_BACKEND_URL || "undefined";
+  process.env.REACT_APP_BACKEND_URL|| "undefined";
 const BACKEND_WS = process.env.REACT_APP_WS_URL || "undefined";
 const API_KEY = process.env.REACT_APP_API_KEY || "undefined";
 
