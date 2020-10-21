60cecb4c7d26a40c3ed191077c94a59882572045
Alex Bean <alexfraga10@gmail.com>
2020-10-21 22:46:59 +0200

    change frontend ajuntament de gava

--- implementations/ajuntament-gava/ajuntament-gava-frontend/src/components/Footer/Footer.tsx
+++ implementations/ajuntament-gava/ajuntament-gava-frontend/src/components/Footer/Footer.tsx
@@ -7,21 +7,23 @@ class Footer extends Component {
       <footer>
 	
 	 
-		<div id="footerPri">
+		<div className="footerPri">
 	
 	
 	
-	<div id="FOOTER_SINMENU_PTS" className="FOOTER widget ">
+	<div className="FOOTER_SINMENU_PTS FOOTER widget ">
 			
 			<div id="aazone.FOOTER_SINMENU_PTS"><div id="footerTop"></div>
 		<div id="footerContent">
 		<ul className="footer-wrapper">
-			<li className="middle"><div id="datos_pie"><strong><span className="tit">Ajuntament de Gavà<br /></span></strong><span className="bla">Plaça de Jaume Balmes S\N, 08850 Gavà (Barcelona)<br/>Telèfon: 932 639 100<br/></span></div>
+
+        <div id="datos_pie"><strong><span className="tit">Ajuntament de Gavà<br /></span></strong><span className="bla">Plaça de Jaume Balmes S\N, 08850 Gavà (Barcelona)<br/>Telèfon: 932 639 100<br/></span></div>
+
 <div id="menu_pie">
 
 <div id="pie_listado">
 <ul className="bla">
-<li><a title="" href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_AVISOLEGAL">Avís legal</a></li>
+<li><a title="" href="#">Avís legal</a></li>
 <li><a title="" href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_PROTECDATA">Política de privacitat</a></li>
 <li><a title="" href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_COOKIES">Política de cookies</a></li>
 <li><a title="" href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_ACCESIBIL">Accessibilitat</a></li>
@@ -33,7 +35,7 @@ class Footer extends Component {
 <li><a title="" href="http://jigsaw.w3.org/css-validator/" target="_blank"><img src={require("../../assets/images/0_30_1.png")} alt="Accesibilidad CSS" title="Accessibilitat CSS" /></a></li> */}
 </ul>
 </div>
-</div></li>
+</div>
 			</ul>	
 		</div>
 </div></div>
