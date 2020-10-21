a549695f50012d552eb157d032fd7c84c3752b67
Alex Bean <alexfraga10@gmail.com>
2020-10-21 18:43:10 +0200

    change footer and header

--- implementations/ajuntament-gava/ajuntament-gava-frontend/src/components/Footer/Footer.tsx
+++ implementations/ajuntament-gava/ajuntament-gava-frontend/src/components/Footer/Footer.tsx
@@ -4,50 +4,48 @@ import "./Footer.css";
 class Footer extends Component {
   render() {
     return (
-      <footer className="mt-4">
-        <div className="footer-main">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-6">
-                <div className="media footer-media">
-                  <img
-                    className="align-self-center mr-3"
-                    src={require("../../assets/images/logoCity.png")}
-                    alt="City"
-                  />
-                  <div className="media-body align-self-center">
-                    <h3 className="mt-0">
-                      The official website of Government of Freedonia
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6">
-                <div className="footer-main-right">
-                  <div className="footer-social-links">
-                    <a href="#">
-                      <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="#">
-                      <i className="fab fa-facebook"></i>
-                    </a>
-                    <a href="#">
-                      <i className="fab fa-youtube"></i>
-                    </a>
-                  </div>
-                  <div className="footer-contact">
-                    <h4>Contact Info</h4>
-                  </div>
-                  <div className="footer-contact-links">
-                    <a href="#">info@city.gov</a>
-                    <a href="#">(312)-774-2828</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <footer>
+	
+	 
+		<div id="footerPri">
+	
+	
+	
+	<div id="FOOTER_SINMENU_PTS" className="FOOTER widget ">
+			
+			<div id="aazone.FOOTER_SINMENU_PTS"><div id="footerTop"></div>
+		<div id="footerContent">
+		<ul className="footer-wrapper">
+			<li className="middle"><div id="datos_pie"><strong><span className="tit">Ajuntament de Gavà<br /></span></strong><span className="bla">Plaça de Jaume Balmes S\N, 08850 Gavà (Barcelona)<br/>Telèfon: 932 639 100<br/></span></div>
+<div id="menu_pie">
+
+<div id="pie_listado">
+<ul className="bla">
+<li><a title="" href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_AVISOLEGAL">Avís legal</a></li>
+<li><a title="" href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_PROTECDATA">Política de privacitat</a></li>
+<li><a title="" href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_COOKIES">Política de cookies</a></li>
+<li><a title="" href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_ACCESIBIL">Accessibilitat</a></li>
+</ul>
+</div>
+<div id="pie_listado2">
+<ul className="bla2">
+{/* <li><a title="" href="http://www.w3.org/WAI/WCAG1AA-Conformance" target="_blank"><img src={require("../../assets/images/0_29_1.png")} alt="Accesibilidad AA" title="Accessibilitat AA" /></a></li>
+<li><a title="" href="http://jigsaw.w3.org/css-validator/" target="_blank"><img src={require("../../assets/images/0_30_1.png")} alt="Accesibilidad CSS" title="Accessibilitat CSS" /></a></li> */}
+</ul>
+</div>
+</div></li>
+			</ul>	
+		</div>
+</div></div>
+
+		
+		 
+				
+				
+		</div>
+			
+		
+	</footer>
     );
   }
 }
--- implementations/ajuntament-gava/ajuntament-gava-frontend/src/components/Header/Header.tsx
+++ implementations/ajuntament-gava/ajuntament-gava-frontend/src/components/Header/Header.tsx
@@ -4,200 +4,27 @@ import "./Header.css";
 class Header extends Component {
   render() {
     return (
-      <header className="cds-header" role="banner">
-        <div className="cds-navbar">
-          <button className="cds-menu-btn">MENU</button>
-          <div className="cds-logo" id="logo">
-            <em className="cds-logo-text">
-              <a href="/demo/gavius" title="Home">
-                <img
-                  src={require("../../assets/images/logoCity.png")}
-                  alt="Logo of the City"
-                />
-                <h1 className="cds-header-title">
-                  &nbsp;&nbsp;&nbsp;Government of Freedonia
-                </h1>
-              </a>
-            </em>
-          </div>
-        </div>
-
-        <nav role="navigation" className="cds-nav">
-          <div className="cds-nav-inner">
-            <button className="cds-nav-close">
-              <img
-                src="https://webapps1.chicago.gov/cdn/chiwds/0.9.1/img/close.svg"
-                alt="close"
-              />
-            </button>
-            <ul className="cds-nav-primary cds-accordion">
-              <li>
-                <a className="cds-nav-link" href="/demo/gavius">
-                  <span>Home</span>
-                </a>
-              </li>
-              <li>
-                <button
-                  className="cds-accordion-button cds-nav-link"
-                  aria-expanded="false"
-                  aria-controls="nav-pi"
-                >
-                  <span>Programs & Initiatives</span>
-                </button>
-                <div
-                  id="nav-pi"
-                  className="cds-nav-submenu cds-nav-megamenu row"
-                  aria-hidden="true"
-                >
-                  <div className="col-md-6">
-                    <ul>
-                      <li>
-                        <a title="Affordable Barcelona" href="#">
-                          Affordable City
-                        </a>
-                        <a title="Barcelona Census 2020" href="#">
-                          City Census 2020
-                        </a>
-                        <a title="Consumer Protection" href="#">
-                          Consumer Protection
-                        </a>
-                        <a title="Education" href="#">
-                          Education
-                        </a>
-                        <a title="Environment" href="#">
-                          Environment
-                        </a>
-                        <a title="Food Service Establishments" href="#">
-                          Food Service Establishments
-                        </a>
-                        <a title="Freedom of Information (FOIA)" href="#">
-                          Freedom of Information (FOIA)
-                        </a>
-                        <a title="Grants" href="#">
-                          Grants
-                        </a>
-                        <a title="Grounds for Peace" href="#">
-                          Grounds for Peace
-                        </a>
-                        <a title="Health &amp; Wellness" href="#">
-                          Health &amp; Wellness
-                        </a>
-                        <a title="Housing" href="#">
-                          Housing
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="col-md-6">
-                    <ul>
-                      <li>
-                        <a
-                          title="Inspections, Permitting &amp; Licensing"
-                          href="#"
-                        >
-                          Inspections, Permitting &amp; Licensing
-                        </a>
-                        <a title="Jobs" href="#">
-                          Jobs
-                        </a>
-                        <a title="Municipal Marketing" href="#">
-                          Municipal Marketing
-                        </a>
-                        <a title="Our City. Our Safety." href="#">
-                          Our City. Our Safety.
-                        </a>
-                        <a title="Safety" href="#">
-                          Safety
-                        </a>
-                        <a title="Sustain Chicago" target="_blank" href="#">
-                          Sustain City
-                        </a>
-                        <a title="Taxes" href="#">
-                          Taxes
-                        </a>
-                        <a title="Technology" href="#">
-                          Technology
-                        </a>
-                        <a title="Transparency" href="#">
-                          Transparency
-                        </a>
-                        <a title="Transportation" href="#">
-                          Transportation
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <button
-                  className="cds-accordion-button cds-nav-link"
-                  aria-expanded="false"
-                  aria-controls="nav-5"
-                >
-                  <span>Government</span>
-                </button>
-                <ul id="nav-5" className="cds-nav-submenu" aria-hidden="true">
-                  <li>
-                    <a href="#">Page title</a>
-                  </li>
-                  <li>
-                    <a href="#">Page title one</a>
-                  </li>
-                  <li>
-                    <a href="#">Page title two</a>
-                  </li>
-                  <li>
-                    <a href="#">Link</a>
-                  </li>
-                </ul>
-              </li>
-
-              <li>
-                <button
-                  className="cds-accordion-button cds-nav-link"
-                  aria-expanded="false"
-                  aria-controls="nav-about"
-                >
-                  <span>About</span>
-                </button>
-                <ul
-                  id="nav-about"
-                  className="cds-nav-submenu"
-                  aria-hidden="true"
-                >
-                  <li>
-                    <a title="Chicago History" href="#">
-                      City History
-                    </a>
-                  </li>
-                  <li>
-                    <a title="Facts &amp; Statistics" href="#">
-                      Facts &amp; Statistics
-                    </a>
-                  </li>
-                  <li>
-                    <a title="Attractions" href="#" target="_blank">
-                      Attractions
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <div className="cds-nav-secondary">
-              <ul className="cds-unstyled-list cds-nav-secondary-links">
-                <li>
-                  <a href="#">
-                    <span>Accessibility </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">External link</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+      <header>
+		
+      <div id="cabeceraPri"> 
+      
+      <div id="HEADER_PTS" className="HEADER widget ">
+        
+        <div id="aazone.HEADER_PTS"><div id="logo">
+  <a href="../../sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_HOME"><img src={require("../../assets/images/cabe.jpg")} alt="Seu electrònica del Ajuntament" /></a></div>
+  <div id="textosHeader">
+      <h1>
+      <a href="https://eseu.gava.cat/sta/CarpetaPublic/doEvent?APP_CODE=STA&amp;PAGE_CODE=PTS_HOME">
+          &nbsp;</a>
+        </h1>	
+      <h2>&nbsp;</h2>
+      </div>
+    </div></div></div>
+       
+        <div className="clear"></div>
+        
+      
+    
       </header>
     );
   }
