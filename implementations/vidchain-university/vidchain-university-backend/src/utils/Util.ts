a3dcf4d305c949e1912449ea974c15545d6d3887
Alex Bean <alexfraga10@gmail.com>
2020-11-13 18:45:04 +0100

    extractVCfromPresentation

--- implementations/vidchain-university/vidchain-university-backend/src/utils/Util.ts
+++ implementations/vidchain-university/vidchain-university-backend/src/utils/Util.ts
@@ -48,12 +48,16 @@ function strB64enc(input) {
 
 function extractVCfromPresentation(credential) {
   let jwtObject = strB64dec(credential.data.decrypted);
-  jwtObject = jwtObject.substring(
-    jwtObject.lastIndexOf("[") + 1,
-    jwtObject.lastIndexOf("]")
-  );
-  jwtObject = jwtObject.substring(1, jwtObject.length - 1);
-  return decodeJWT(jwtObject);
+  // console.log("read jwtob");
+  // console.log(jwtObject);
+  // console.log("--");
+  // console.log(JSON.stringify(jwtObject));
+  // jwtObject = jwtObject.substring(
+  //   jwtObject.lastIndexOf("[") + 1,
+  //   jwtObject.lastIndexOf("]")
+  // );
+  // jwtObject = jwtObject.substring(1, jwtObject.length - 1);
+  return decodeJWT(jwtObject.verifiableCredential[0]);
 }
 
 export { decodeJWT, parseJwt, strB64dec, strB64enc, extractVCfromPresentation };
