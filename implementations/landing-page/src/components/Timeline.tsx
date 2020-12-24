63f8ebc498c708af114b54244bf1b025562de0f2
Mauro Lucchini <maurolucchinidepompa@gmail.com>
2020-12-24 15:28:22 +0100

    fix: fit timeline with four steps

--- implementations/landing-page/src/components/Timeline.tsx
+++ implementations/landing-page/src/components/Timeline.tsx
@@ -20,7 +20,7 @@ const Timeline = ({
         <a href="#download">
             <div className={getStep(step,0)}>
                 <h1>Step 0</h1>
-                <h2>Download VIDwallet</h2>
+                <h3>Download VIDwallet</h3>
                 <div className="img-container">
                     <img src={logoVidchain} className="logoTimeline" alt="vidchain logo"/> 
                     <p>Install VIDwallet App<br/>in your smartphone</p>
@@ -30,30 +30,30 @@ const Timeline = ({
         <a href="#download">
             <div className={getStep(step,1)}>
                 <h1>Step 1</h1>
-                <h2>Get your Verifiable eID</h2>
+                <h3>Get your Verifiable eID</h3>
                 <div className="img-container">
                     <img src={kyc} className="logoTimeline" alt="vidchain logo"/> 
-                    <p>Get your first credential by proving<br/> liveliness and verifying your identity</p>
+                    <p>Get your first credential <br/>by proving liveliness <br/>and verifying your identity</p>
                 </div>
             </div>
         </a>
         <a href="/demo/government">
             <div className={getStep(step,2)}>
                 <h1>Step 2</h1>
-                <h2>Government of Freedonia</h2>
+                <h3>Government of Freedonia</h3>
                 <div className="img-container">
                     <img src={logoGovernment} className="logoTimeline" alt="government logo"/> 
-                    <p>Access to Government of Freedonia<br/>with VIDchain and request Large Family Credential</p>
+                    <p>Access with VIDchain <br/>and<br/> request Large Family Credential</p>
                 </div>
             </div>
         </a>
         <a href="/demo/university">
             <div className={getStep(step,3)}>
             <h1>Step 3</h1>
-                <h2>University</h2>
+                <h3>ACME University</h3>
                 <div className="img-container">
                     <img src={logoUniversity} className="logoTimeline" alt="university logo"/> 
-                    <p>Access to ACME university and request a discount<br/>  presenting your Large Family Credential</p>
+                    <p>Access with VIDchain and <br/>request a discount<br/>  presenting<br/>Large Family Credential</p>
                 </div>
             </div>
         </a>
