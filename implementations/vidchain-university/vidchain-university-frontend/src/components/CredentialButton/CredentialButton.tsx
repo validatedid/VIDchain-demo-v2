007db206cbea1e06995d944420140b17b3020f4c
Alex Bean <alexfraga10@gmail.com>
2021-01-25 22:22:48 +0100

    button credential and panel components

---
+++ implementations/vidchain-university/vidchain-university-frontend/src/components/CredentialButton/CredentialButton.tsx
@@ -0,0 +1,24 @@
+import {Button} from '@material-ui/core';
+import { withStyles} from '@material-ui/core/styles';
+
+export const CredentialButton = withStyles((theme) => ({
+    root: {
+    padding: '1%',
+    display: 'block',
+    borderStyle: 'none',
+    width: '285.17px',
+    height: '46px',
+    background: '#E61B72',
+    borderRadius: '4px',
+    color: '#ffffff',
+    left: '25%',
+    marginTop: '2%',
+    marginBottom: '5%',
+    fontSize: '16px',
+    lineHeight: '19px',
+    fontStyle: 'normal',
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    },
+  }))(Button);
+  
