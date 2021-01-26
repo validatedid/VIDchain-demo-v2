4516e1a1233b4a29f2baa2235ebd1e01434bab97
Alex Bean <alexfraga10@gmail.com>
2021-01-26 12:46:23 +0100

    footer responsive

--- implementations/vidchain-government/vidchain-government-frontend/src/components/Footer/Footer.tsx
+++ implementations/vidchain-government/vidchain-government-frontend/src/components/Footer/Footer.tsx
@@ -7,16 +7,16 @@ class Footer extends Component {
   render() {
     return (
         <Grid container className="footer" spacing={6}>
-          <Grid item sm={1}></Grid>
-          <Grid item xs={12} sm={3} className="logoFooterDiv">
+          <Grid item xs={1} sm={1}></Grid>
+          <Grid item xs={3} sm={3} className="logoFooterDiv">
               <img
                 className="logoFooter"
                 src={require("../../assets/images/logo.svg")}
                 alt="City"
               />
             </Grid>
-            <Grid item sm={4}></Grid>
-            <Grid item xs={12} sm={3}>
+            <Grid item xs={1} sm={4}></Grid>
+            <Grid item xs={3} sm={3}>
               <p className="textFooter">This is not an official website of any Government.</p>
             </Grid>
         </Grid>
