ecf66e5d07e7507cc30ce0185b091153cde51b11
Alex Bean <alexfraga10@gmail.com>
2021-01-26 09:22:47 +0100

    footer university

--- implementations/vidchain-university/vidchain-university-frontend/src/components/Footer/Footer.tsx
+++ implementations/vidchain-university/vidchain-university-frontend/src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ class Footer extends Component {
   render() {
     return (
         <Grid container className="footer" spacing={6}>
-          <Grid item sm={3}></Grid>
+          <Grid item sm={1}></Grid>
           <Grid item xs={12} sm={3} className="logoFooterDiv">
               <img
                 className="logoFooter"
@@ -15,8 +15,9 @@ class Footer extends Component {
                 alt="City"
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1" className="textFooter">This is not an official website of any University.</Typography>
+            <Grid item sm={4}></Grid>
+            <Grid item xs={12} sm={3}>
+              <p className="textFooter">This is not an official website of any University.</p>
             </Grid>
         </Grid>
     );
