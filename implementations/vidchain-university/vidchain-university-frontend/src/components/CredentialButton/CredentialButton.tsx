87718586fb5c8f14316c933d373dcb1c13bdb17c
Alex Bean <alexfraga10@gmail.com>
2021-01-26 13:59:39 +0100

    profile university responsive

--- implementations/vidchain-university/vidchain-university-frontend/src/components/CredentialButton/CredentialButton.tsx
+++ implementations/vidchain-university/vidchain-university-frontend/src/components/CredentialButton/CredentialButton.tsx
@@ -11,7 +11,7 @@ export const CredentialButton = withStyles((theme) => ({
     background: '#9D56BE',
     borderRadius: '4px',
     color: '#ffffff',
-    left: '25%',
+    alignSelf: 'center',
     marginTop: '2%',
     marginBottom: '5%',
     fontSize: '16px',
