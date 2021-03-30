2fd474576bef8a1d58dda688f816e2b16717a32f
Alex Bean <alexfraga10@gmail.com>
2021-03-30 16:22:06 +0200

    redesign home screen in health center

--- implementations/vidchain-airline/vidchain-airline-frontend/src/screens/Home/Home.tsx
+++ implementations/vidchain-airline/vidchain-airline-frontend/src/screens/Home/Home.tsx
@@ -61,7 +61,7 @@ class Home extends Component<Props, State> {
           className="content">
           <div className="subcontent"></div>
         <Grid item>
-          <Typography variant="h2"className="title">Welcome to Oceanic Airlines</Typography> 
+          <Typography variant="h2" className="title">Welcome to Oceanic Airlines</Typography> 
           <Typography variant="h5" className="subtitle"><b>We're happy to have you onboard. Login to your site to get your tickets.</b></Typography>
           
           {isQRdisplayed && 
