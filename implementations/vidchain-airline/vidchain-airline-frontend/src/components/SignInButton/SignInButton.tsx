5cb7493627d987d271d65b0416c26d00a4032263
Alex Bean <alexfraga10@gmail.com>
2021-03-30 12:28:39 +0200

    main screen design airline

--- implementations/vidchain-airline/vidchain-airline-frontend/src/components/SignInButton/SignInButton.tsx
+++ implementations/vidchain-airline/vidchain-airline-frontend/src/components/SignInButton/SignInButton.tsx
@@ -3,22 +3,16 @@ import { withStyles} from '@material-ui/core/styles';
 
 export const SignInButton = withStyles((theme) => ({
     root: {
-    paddingButton: '2%',
-    height: '8%',
-    position: 'absolute',
-    marginTop: '2%',
-    display: 'block',
-    borderStyle: 'none',
-    borderRadius: '1px',
-    backgroundColor: '#6e3188',
-    boxShadow: 'none',
-    textShadow: 'none',
-    color: '#f7f7f7',
-    fontSize: '16px',
-    lineHeight: '19px',
-    fontStyle: 'normal',
-    textAlign: 'center',
-    textTransform: 'capitalize',
+      width: '248px',
+      height: '46px',
+      backgroundColor: '#2F61FF',
+      bordeRadius: '4px',
+      color: '#FFFFFF',
+      fontSize: '18px',
+      lineHeight: '19px',
+      fontStyle: 'normal',
+      textAlign: 'center',
+      textTransform: 'capitalize',
     },
   }))(Button);
   
--- implementations/vidchain-airline/vidchain-airline-frontend/src/screens/Home/Home.tsx
+++ implementations/vidchain-airline/vidchain-airline-frontend/src/screens/Home/Home.tsx
@@ -59,10 +59,11 @@ class Home extends Component<Props, State> {
           justify="space-between"
           alignItems="flex-start"
           className="content">
-        <Grid item>
           <div className="subcontent"></div>
+        <Grid item>
           <Typography variant="h2"className="title">Welcome to Oceanic Airlines</Typography> 
           <Typography variant="h5" className="subtitle"><b>We're happy to have you onboard. Login to your site to get your tickets.</b></Typography>
+          
           {isQRdisplayed && 
                 <div className="QRDidKeys">
                   <Typography variant="h5" className="scanTitle"><b>Scan the QR code to login.</b></Typography>
